7a42b5d13d4618132f3cd91d55ef825695920a3b
Mariusz Fit <72906237+fitmariusz@users.noreply.github.com>
2024-04-21 01:02:41 +0200

    finish

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,16 +1,33 @@
 import { searchContact } from "../Search/Search";
+import css from "./ContactsList.module.css"
+
+
+const showContactsList = ({ dataPhonebook, onDelete }) => {
+  const contacstLIstSearch = searchContact(dataPhonebook.contacts, dataPhonebook.filter)
+  return <>
+            <li className={css.li}><span>Name</span><span>Number</span><span>      </span></li>
+            {contacstLIstSearch.map(contact =>
+              <li className={css.li} key={contact.id}><span className={css.span}>{contact.name}</span><span className={css.span} >{contact.number}</span>
+                <button className={css.button} onClick={() => onDelete(contact.id)} required>Delete</button></li>)}
+  </>
+              
+
+};
+
+
 
 export const ContactList = ({ dataPhonebook, onDelete, onChange }) => { 
-
   return (<>
-      <input type="search" name="filter" onChange={onChange} placeholder='Search'></input>
-    <ul>
+    <div className={css.contener}>
+      <input className={css.input} type="search" name="filter" onChange={onChange} placeholder='Search'></input>
+    <ul className={css.ul}>
           {searchContact(dataPhonebook.contacts, dataPhonebook.filter).length !== 0 ? 
-            searchContact(dataPhonebook.contacts, dataPhonebook.filter).map(contact =>
-              <li key={contact.id}><span>{contact.name}{": "}</span><span>{contact.number}{" "}</span>
-                <button onClick={() => onDelete(contact.id)} required>Delete</button></li>)
+            showContactsList(dataPhonebook={dataPhonebook}, onDelete = {onDelete})
             :
           <p>No contacts....</p>}
       </ul>
+      </div>
     </>);
 };
+
+
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -6,11 +6,13 @@ export const Form = ({ dataPhonebook, onSubmit, onChange }) => {
         <>
         <form onSubmit={onSubmit} className={css.contactForm}>
       <label htmlFor='nameContact'>Name</label> 
-        <input className={css.input} id="nameContact" type="text" name="name" value={ dataPhonebook.value} onChange={onChange} pattern="[A-Za-z]{1,32}" required placeholder="Name"></input>
+        <input className={css.input} id="nameContact" type="text" name="name" value={ dataPhonebook.value} onChange={onChange} pattern="^[a-zA-Zа-яА-Я]*$" required placeholder="Name"></input>
         <label htmlFor='numberContact'>Number phone</label> 
         <input className={css.input} id="numberContact" type="tel" name="number" value={ dataPhonebook.value} onChange={onChange} pattern="\+?\d{1,4}?[ .\\-\\s]?\(?\d{1,3}?\)?[ .\\-\\s]?\d{1,4}[ .\\-\\s]?\d{1,4}[ .\\-\\s]?\d{1,9}" required placeholder="Number"></input>
-        <button className={css.button} type="submit">Add</button>
+        <button className={css.button} type="submit">Add contact</button>
       </form>
         </>
     )
- };
+};
+ 
+// pattern="^[a-zA-Zа-яА-Я]+(([' \\-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
